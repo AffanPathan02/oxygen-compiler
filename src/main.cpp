820e77ef759bb9882c72bfe44b332a0cee419296
Affan Pathan <pathan.affan02@gmail.com>
2023-12-12 21:21:10 +0530

    Implement parse tree, parser, and generator.

--- src/main.cpp
+++ src/main.cpp
@@ -1,119 +1,46 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
+#include <fstream>
+#include <iostream>
 #include <optional>
+#include <sstream>
 #include <vector>
 
-using namespace std;
+#include "./generation.h"
 
-enum class TokenType {
-    _return,
-    int_lit,
-    semi
-};
-
-struct Token{
-    TokenType type;
-    optional<string> value{};
-};
-
-vector<Token> tokenize(const string& str){
-    std::vector<Token> tokens;
-
-    std::string buf;
-    for (int i = 0; i < str.length(); i++)
-    {
-        char c = str.at(i);
-        if (std::isalpha(c))
-        {
-            buf.push_back(c);
-            i++;
-            while (std::isalnum(str.at(i)))
-            {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            if (buf == "return")
-            {
-                tokens.push_back({.type = TokenType::_return});
-                buf.clear();
-                continue;
-            } else
-            {
-                std::cerr << "You messed up!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        } else if (std::isdigit(c))
-        {
-            buf.push_back(c);
-            i++;
-            while (std::isdigit(str.at(i)))
-            {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_lit, .value = buf});
-            buf.clear();
-        } else if (c == ';')
-        {
-            tokens.push_back({.type = TokenType::semi});
-        } else if (std::isspace(c))
-        {
-            continue;
-        } else
-        {
-            std::cerr << "You messed up!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-    return tokens;
-};
-
-string tokens_to_asm(const vector<Token>& tokens){
-    stringstream output;
-    output<< "global _start\n_start:\n ";
-    for (int i = 0; i < tokens.size(); i++) {
-        const Token& token = tokens.at(i);
-        if(token.type == TokenType::_return){
-            if(i+1 <tokens.size() && tokens.at(i+1).type==TokenType::int_lit){
-                if(i+2 < tokens.size() && tokens.at(i+2).type == TokenType::semi){
-                    output << "    mov rax, 60\n";
-                    output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
-                    output << "    syscall";
-                }
-            }
-        }
-    }
-    return output.str();
-}
-
-int main(int argc, char* argv[]){
-    if (argc != 2){
-        cerr << "Incorrect Usage. Correct Usage .."<<endl;
-        cerr <<"oxy <input.ox>"<<endl;
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+        std::cerr << "oxy <oxy.ox>" << std::endl;
         return EXIT_FAILURE;
     }
 
-    string contents;
+    std::string contents;
     {
-        stringstream contents_stream;
-        fstream input(argv[1] ,ios::in);
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
         contents_stream << input.rdbuf();
-        contents=contents_stream.str();
+        contents = contents_stream.str();
     }
-    std::vector<Token> tokens = tokenize(contents);
 
+    Tokenizer tokenizer(std::move(contents));
+    std::vector<Token> tokens = tokenizer.tokenize();
+
+    Parser parser(std::move(tokens));
+    std::optional<NodeExit> tree = parser.parse();
+
+    if (!tree.has_value()) {
+        std::cerr << "No exit statement found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Generator generator(tree.value());
     {
-        fstream file("out.asm",ios::out);
-        file<<tokens_to_asm(tokens);
+        std::fstream file("out.asm", std::ios::out);
+        file << generator.generate();
     }
-
 
     system("nasm -felf64 out.asm");
     system("ld -o out out.o");
 
-//    cout<<contents<<endl;
     return EXIT_SUCCESS;
 }
